28ece0423f5afd416621943c02df02bbf8c3686f
Mario <suk.mario@gmail.com>
2019-01-29 00:41:34 +0100

    Fix return type of MakeRequest method

--- AhrefsClient/AhrefsWrapper.cs
+++ AhrefsClient/AhrefsWrapper.cs
@@ -1,4 +1,5 @@
 ﻿using AhrefsClient.Enums;
+using AhrefsClient.Mappers;
 using AhrefsClient.Models;
 using AhrefsClient.Models.Responses;
 using Newtonsoft.Json;
@@ -23,14 +24,14 @@ namespace AhrefsClient
         }
 
 
-        public void MakeRequest(AhrefsQuery ahrefsQuery)
+        public object MakeRequest(AhrefsQuery ahrefsQuery)
         {
             ahrefsQuery.SetToken(_ahrefsAuthentication.GetToken());
             var builder = new UriBuilder(AhrefsDomain.GetDomain(_ahrefsEndPoints));
             builder.Query = ahrefsQuery.GetQueryString().ToString();
 
             var rawResponse = SendRequest(builder.ToString());
-            var result = DeserializeObject(rawResponse, ahrefsQuery.From);
+            return DeserializeObject(rawResponse, ahrefsQuery.From);
         }
 
         private string SendRequest(string url)
@@ -56,91 +57,12 @@ namespace AhrefsClient
 
         private object DeserializeObject(string ObjectToDeserialize, AhrefsFrom from)
         {
-            var classType = GetClassType(from);
+            var classType = AhrefsFromMapper.GetClassType(from);
             var deserializedObject = JsonConvert.DeserializeObject(ObjectToDeserialize, classType);
             return deserializedObject;
         }
 
-        private Type GetClassType(AhrefsFrom from)
-        {
-            Type returnClass = null;
-            switch (from)
-            {
-                case AhrefsFrom.ahrefs_rank:
-                    returnClass = typeof(AhrefsRank);
-                    break;
-                case AhrefsFrom.anchors:
-                    returnClass = typeof(AhrefsAnchors);
-                    break;
-                case AhrefsFrom.anchors_refdomains:
-                    returnClass = typeof(AhrefsRefDomains);
-                    break;
-                case AhrefsFrom.backlinks:
-                    returnClass = typeof(AhrefsBacklinks);
-                    break;
-                case AhrefsFrom.backlinks_new_lost:
-                    returnClass = typeof(AhrefsBacklinksNewLost);
-                    break;
-                case AhrefsFrom.backlinks_new_lost_counters:
-                    returnClass = typeof(AhrefsBacklinksNewLostCounters);
-                    break;
-                case AhrefsFrom.backlinks_one_per_domain:
-                    returnClass = typeof(AhrefsBacklinksOnePerDomain);
-                    break;
-                case AhrefsFrom.broken_backlinks:
-                    returnClass = typeof(AhrefsBacklinks);
-                    break;
-                case AhrefsFrom.broken_links:
-                    returnClass = typeof(AhrefsBrokenLinks);
-                    break;
-                case AhrefsFrom.domain_rating:
-                    returnClass = typeof(AhrefsDomainRating);
-                    break;
-                case AhrefsFrom.linked_anchors:
-                    returnClass = typeof(AhrefsLinkedAnchors);
-                    break;
-                case AhrefsFrom.linked_domains:
-                    returnClass = typeof(AhrefsLinkedDomains);
-                    break;
-                case AhrefsFrom.linked_domains_by_type:
-                    returnClass = typeof(AhrefsLinkedDomainsByType);
-                    break;
-                case AhrefsFrom.metrics:
-                    returnClass = typeof(AhrefsMetrics);
-                    break;
-                case AhrefsFrom.metrics_extended:
-                    returnClass = typeof(AhrefsMetricsExtended);
-                    break;
-                case AhrefsFrom.pages:
-                    returnClass = typeof(AhrefsPages);
-                    break;
-                case AhrefsFrom.pages_extended:
-                    returnClass = typeof(AhrefsPagesExtended);
-                    break;
-                case AhrefsFrom.pages_info:
-                    returnClass = typeof(AhrefsPagesInfo);
-                    break;
-                case AhrefsFrom.refdomains:
-                    returnClass = typeof(AhrefsRefDomains);
-                    break;
-                case AhrefsFrom.refdomains_by_type:
-                    returnClass = typeof(AhrefsRefDomainsByType);
-                    break;
-                case AhrefsFrom.refdomains_new_lost:
-                    returnClass = typeof(AhrefsRefDomainsNewLost);
-                    break;
-                case AhrefsFrom.refdomains_new_lost_counters:
-                    returnClass = typeof(AhrefsRefDomainsNewLostCounters);
-                    break;
-                case AhrefsFrom.refips:
-                    returnClass = typeof(AhrefsRefips);
-                    break;
-                case AhrefsFrom.subscription_info:
-                    returnClass = typeof(AhrefsSubscriptionInfo);
-                    break;
-            }
-            return returnClass;
-        }
+        
 
     }
 }
---
+++ AhrefsClient/Mappers/AhrefsFromMapper.cs
@@ -0,0 +1,92 @@
+﻿using AhrefsClient.Enums;
+using AhrefsClient.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AhrefsClient.Mappers
+{
+    public static class AhrefsFromMapper
+    {
+        public static Type GetClassType(AhrefsFrom from)
+        {
+            Type returnClass = null;
+            switch (from)
+            {
+                case AhrefsFrom.ahrefs_rank:
+                    returnClass = typeof(AhrefsRank);
+                    break;
+                case AhrefsFrom.anchors:
+                    returnClass = typeof(AhrefsAnchors);
+                    break;
+                case AhrefsFrom.anchors_refdomains:
+                    returnClass = typeof(AhrefsRefDomains);
+                    break;
+                case AhrefsFrom.backlinks:
+                    returnClass = typeof(AhrefsBacklinks);
+                    break;
+                case AhrefsFrom.backlinks_new_lost:
+                    returnClass = typeof(AhrefsBacklinksNewLost);
+                    break;
+                case AhrefsFrom.backlinks_new_lost_counters:
+                    returnClass = typeof(AhrefsBacklinksNewLostCounters);
+                    break;
+                case AhrefsFrom.backlinks_one_per_domain:
+                    returnClass = typeof(AhrefsBacklinksOnePerDomain);
+                    break;
+                case AhrefsFrom.broken_backlinks:
+                    returnClass = typeof(AhrefsBacklinks);
+                    break;
+                case AhrefsFrom.broken_links:
+                    returnClass = typeof(AhrefsBrokenLinks);
+                    break;
+                case AhrefsFrom.domain_rating:
+                    returnClass = typeof(AhrefsDomainRating);
+                    break;
+                case AhrefsFrom.linked_anchors:
+                    returnClass = typeof(AhrefsLinkedAnchors);
+                    break;
+                case AhrefsFrom.linked_domains:
+                    returnClass = typeof(AhrefsLinkedDomains);
+                    break;
+                case AhrefsFrom.linked_domains_by_type:
+                    returnClass = typeof(AhrefsLinkedDomainsByType);
+                    break;
+                case AhrefsFrom.metrics:
+                    returnClass = typeof(AhrefsMetrics);
+                    break;
+                case AhrefsFrom.metrics_extended:
+                    returnClass = typeof(AhrefsMetricsExtended);
+                    break;
+                case AhrefsFrom.pages:
+                    returnClass = typeof(AhrefsPages);
+                    break;
+                case AhrefsFrom.pages_extended:
+                    returnClass = typeof(AhrefsPagesExtended);
+                    break;
+                case AhrefsFrom.pages_info:
+                    returnClass = typeof(AhrefsPagesInfo);
+                    break;
+                case AhrefsFrom.refdomains:
+                    returnClass = typeof(AhrefsRefDomains);
+                    break;
+                case AhrefsFrom.refdomains_by_type:
+                    returnClass = typeof(AhrefsRefDomainsByType);
+                    break;
+                case AhrefsFrom.refdomains_new_lost:
+                    returnClass = typeof(AhrefsRefDomainsNewLost);
+                    break;
+                case AhrefsFrom.refdomains_new_lost_counters:
+                    returnClass = typeof(AhrefsRefDomainsNewLostCounters);
+                    break;
+                case AhrefsFrom.refips:
+                    returnClass = typeof(AhrefsRefips);
+                    break;
+                case AhrefsFrom.subscription_info:
+                    returnClass = typeof(AhrefsSubscriptionInfo);
+                    break;
+            }
+            return returnClass;
+        }
+    }
+}
